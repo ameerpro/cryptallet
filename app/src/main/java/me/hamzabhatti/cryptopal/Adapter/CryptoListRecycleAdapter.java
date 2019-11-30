a2d8b90f43f85cccbe2e12c07c1f5747bc404401
Ameer Hamza <ameer.hamza@logicon.com.pk>
2019-11-30 19:27:44 +0500

    First Push. Full Project Added

---
+++ app/src/main/java/me/hamzabhatti/cryptopal/Adapter/CryptoListRecycleAdapter.java
@@ -0,0 +1,120 @@
+package me.hamzabhatti.cryptopal.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import me.hamzabhatti.cryptopal.ModelClass.CryptoListModelClass;
+
+import me.hamzabhatti.cryptopal.AlertCryptoStarActivity;
+import me.hamzabhatti.cryptopal.ChartCryptoStarActivity;
+import me.hamzabhatti.cryptopal.CreatePinAcitivyt;
+import me.hamzabhatti.cryptopal.LoginActivity;
+import me.hamzabhatti.cryptopal.R;
+import me.hamzabhatti.cryptopal.SignupActivity;
+import me.hamzabhatti.cryptopal.SplashScreenActivity;
+import me.hamzabhatti.cryptopal.TradeCryptoStarActivity;
+import me.hamzabhatti.cryptopal.WalletCryptoStarActivity;
+
+
+public class CryptoListRecycleAdapter extends RecyclerView.Adapter<CryptoListRecycleAdapter.MyViewHolder> {
+
+    Context context;
+
+
+    private List<CryptoListModelClass> OfferList;
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+
+        TextView title;
+
+
+        public MyViewHolder(View view) {
+            super(view);
+
+            title = (TextView) view.findViewById(R.id.title);
+
+
+        }
+
+    }
+
+
+    public CryptoListRecycleAdapter(Context context, List<CryptoListModelClass> offerList) {
+        this.OfferList = offerList;
+        this.context = context;
+    }
+
+    @Override
+    public CryptoListRecycleAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_crypto_list, parent, false);
+
+
+        return new CryptoListRecycleAdapter.MyViewHolder(itemView);
+
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
+        CryptoListModelClass lists = OfferList.get(position);
+        holder.title.setText(lists.getTitle());
+
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (position == 0) {
+                    Intent i = new Intent(context, SplashScreenActivity.class);
+                    context.startActivity(i);
+                }else if(position == 1) {
+                    Intent i = new Intent(context, LoginActivity.class);
+                    context.startActivity(i);
+                }else if(position == 2) {
+                    Intent i = new Intent(context, SignupActivity.class);
+                    context.startActivity(i);
+                }else if(position == 3) {
+                    Intent i = new Intent(context, CreatePinAcitivyt.class);
+                    context.startActivity(i);
+                }else if(position == 4) {
+                    Intent i = new Intent(context, TradeCryptoStarActivity.class);
+                    context.startActivity(i);
+                }else if(position == 5) {
+                    Intent i = new Intent(context, WalletCryptoStarActivity.class);
+                    context.startActivity(i);
+                }else if(position == 6) {
+                    Intent i = new Intent(context, AlertCryptoStarActivity.class);
+                    context.startActivity(i);
+                }else if(position == 7) {
+                    Intent i = new Intent(context, ChartCryptoStarActivity.class);
+                    context.startActivity(i);
+                }
+
+            }
+
+        });
+
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return OfferList.size();
+
+    }
+
+}
+
+
